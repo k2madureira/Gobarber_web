f4888df88283a1ec3352dfd3de541de2e3993c29
k2madureira <lenilsonmadureira@gmail.com>
2020-08-27 11:51:11 -0300

    header dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 
-import { Container } from './style';
+import { FiPower } from 'react-icons/fi';
+import { Container, Header, HeaderContent, Profile } from './style';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/Auth';
 
 const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
   return (
-    <>
-      <span>dash</span>
-      <h1> Dashboard</h1>
-    </>
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="logo gobarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+
+            <div>
+              <span>Bem-vido, </span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
   );
 };
 
